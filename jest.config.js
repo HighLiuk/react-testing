fef799b00eb0f9fa658f45cd029f61dd4dac9508
Luca Di Fazio <highliuk@gmail.com>
2022-07-16 17:16:14 +0200

    🔧 React Testing Library

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["./src/jest.setup.ts"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+      tsconfig: "./tsconfig.test.json",
+    },
+  },
 }
