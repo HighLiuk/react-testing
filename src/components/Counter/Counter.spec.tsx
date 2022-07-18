7af146c5fb905421de582e45b672778702faaaa3
Luca Di Fazio <highliuk@gmail.com>
2022-07-19 00:25:46 +0200

    🧪 Add Feature Tests Todos for Incrementor Textbox

--- src/components/Counter/Counter.spec.tsx
+++ src/components/Counter/Counter.spec.tsx
@@ -5,23 +5,39 @@ import Counter from "./Counter"
 describe("Counter", () => {
   beforeEach(() => render(<Counter description="My Counter" />))
 
-  it("should have a description", () => {
-    expect(screen.getByText(/My Counter/i)).toBeInTheDocument()
+  describe("Incrementing and decrementing", () => {
+    it("should have a description", () => {
+      expect(screen.getByText(/My Counter/i)).toBeInTheDocument()
+    })
+
+    it("should start with 0", () => {
+      expect(screen.getByText(/Current Count: 0/i)).toBeInTheDocument()
+    })
+
+    it("should add 1 when the increment button is clicked", async () => {
+      await userEvent.click(screen.getByRole("button", { name: /add/i }))
+
+      expect(screen.getByText(/Current Count: 1/i)).toBeInTheDocument()
+    })
+
+    it("should subtract 1 when the decrement button is clicked", async () => {
+      await userEvent.click(screen.getByRole("button", { name: /subtract/i }))
+
+      expect(screen.getByText(/Current Count: -1/i)).toBeInTheDocument()
+    })
   })
 
-  it("should start with 0", () => {
-    expect(screen.getByText(/Current Count: 0/i)).toBeInTheDocument()
-  })
+  describe("Incrementor Textbox", () => {
+    it.todo("should have an incrementor textbox")
 
-  it("should add 1 when the increment button is clicked", async () => {
-    await userEvent.click(screen.getByRole("button", { name: /add/i }))
+    it.todo("should be able to change the incrementor value")
 
-    expect(screen.getByText(/Current Count: 1/i)).toBeInTheDocument()
-  })
+    it.todo(
+      "should add the incrementor value when the increment button is clicked"
+    )
 
-  it("should subtract 1 when the decrement button is clicked", async () => {
-    await userEvent.click(screen.getByRole("button", { name: /subtract/i }))
-
-    expect(screen.getByText(/Current Count: -1/i)).toBeInTheDocument()
+    it.todo(
+      "should subtract the incrementor value when the decrement button is clicked"
+    )
   })
 })
