629a62e17d2c5b2ea6280e3865addaedf1dc43bc
Luca Di Fazio <highliuk@gmail.com>
2022-07-22 00:12:24 +0200

    🧪 Improved Tests with Jest's SpyOn

--- src/utils/randomBetween.spec.tsx
+++ src/utils/randomBetween.spec.tsx
@@ -1,5 +1,34 @@
 import { randomBetween } from "./randomBetween"
 
+const randomSpy = jest.spyOn(Math, "random")
+
 it("should be greater than 3 when called with min=3 and max=5", () => {
   expect(randomBetween(3, 5)).toBeGreaterThanOrEqual(3)
 })
+
+const tests = [
+  {
+    randomReturn: 0,
+    expectedReturn: 3,
+  },
+  {
+    randomReturn: 0.5,
+    expectedReturn: 4,
+  },
+  {
+    randomReturn: 0.99999,
+    expectedReturn: 5,
+  },
+]
+
+for (const { randomReturn, expectedReturn } of tests) {
+  describe(`when Math.random() returns ${randomReturn}`, () => {
+    beforeEach(() => {
+      randomSpy.mockReturnValue(randomReturn)
+    })
+
+    it(`should return ${expectedReturn} when called with min=3 and max=5`, () => {
+      expect(randomBetween(3, 5)).toBe(expectedReturn)
+    })
+  })
+}
